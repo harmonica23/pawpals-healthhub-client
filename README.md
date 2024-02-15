835aa5a2b4e7ac9ed90b6ee79dcca8f13f9ce1f7
Erica Harmon <ericalaurenharmon@gmail.com>
2024-02-15 15:15:53 -0500

    styling and fontend deployment on vercel

--- README.md
+++ README.md
@@ -5,27 +5,27 @@
 ### **_Getting Started_**
 
 ### **_Links_**
+- [Live Site](https://pawpals-sable.vercel.app/)
 - [Backend Github Repository](https://github.com/harmonica23/pawpals-healthhub-backend)
 - [Trello board](https://trello.com/invite/b/sDoBfhV5/ATTI061ff340b59908a6237f5f11eede5830046BF69B/pawpals-health-hub)
 - [ERD](https://lucid.app/lucidchart/9198234a-8c3c-43a7-b301-56b565ef2d21/edit?invitationId=inv_e1249628-23f3-4d94-9dcb-201c1d048ac5)
 - [Component Hierarchy Diagram](https://lucid.app/lucidchart/7083d453-4e2a-4061-93e7-3516ef4fc1d3/edit?invitationId=inv_b1af0cdd-1099-4f42-83b2-e9f914e00486&page=0_0#)
-- [Live Site]()
 
 ### **_Project Description_**
 **PawPals Health Hub** is a mobile application designed to empower pet owners with a comprehensive tool for tracking and managing their pet's health. With a user-friendly interface, PawPals Health Hub allows individuals to create profiles for their beloved pets, ensuring all vital health information is readily accessible at their fingertips.
 
 ### **_Key Features_**
-**Pet Profile:** Upload a picture of your pet and input essential details including name, birthday, gender, and spay/neuter status.
+**Pet Profile:** Input essential details including name, birthday, gender, and spay/neuter status.
 
 **Diet & Weight Management:** Log your pet's food type, amount, and feeding frequency for a well-maintained diet and track their weight over time.
-
-**Health Incidents:** Record dated health incidents such as injuries, new diagnoses, or symptoms, ensuring a thorough health history.
 
 **Vet Visits:** Log vet visits, categorizing each visit as either sick or healthy. Keep track of medical treatments, procedures, and recommendations in the notes section.
 
 **Vaccine Tracking:** Easily record and monitor your pet's vaccinations, including details like vaccine types, administration dates, and upcoming boosters. 
 
 **Medication Tracking:** Easily recall medications your pet has taken along with dose, frequency, and duration. 
+
+**Health Incidents:** Record dated health incidents such as injuries, new diagnoses, or symptoms, ensuring a thorough health history.
 
 **PawPals Health Hub** aims to simplify the management of your pet's well-being, providing a centralized platform to track their health journey and facilitate effective communication with veterinarians.
 
@@ -40,7 +40,10 @@
 
 
 ### **_Future Updates_**
-
+- Implement feature using GCP and Multer where users can upload a profile picture for each one of their pets.
+- Feature to announce pet's birthday on their profile page if it is, indeed, their birthday!
+- Announcement at the top of the profile page should populate with the pet's most recently entered 
+- Create a new component and render a footer for the project.
 
 
 &copy; 2024 PawPals Health Hub
--- src/components/PetDetail.jsx
+++ src/components/PetDetail.jsx
@@ -14,7 +14,7 @@ const PetDetail = () => {
     const [vax, setVax] = useState([])
     const [meds, setMeds] = useState([])
     const [incidents, setIncidents] = useState([])
-    const [petImage, setPetImage] = useState('')
+    // const [petImage, setPetImage] = useState('')
 
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -99,7 +99,7 @@ const PetDetail = () => {
         const fetchPet = async () => {
             const data = await Client.get(`/pet/${id}`)
             setPet(data.data)
-            setPetImage(data.data.imageUrl)
+            // setPetImage(data.data.imageUrl)
             if (data.data.birthday) {
                 setAge(calculateAge(data.data.birthday))
             }
@@ -157,25 +157,24 @@ const PetDetail = () => {
                 <div className='top'>
                     <h2>All about {pet.name}...</h2>
                     {/* Display pet image here */}
-                    {petImage && (
+                    {/* {petImage && (
                         <img src={petImage} alt={`Image of ${pet.name}`} />
-                    )}
-                    {recentDiet && (
+                    )} */}
+                    {/* {recentDiet && (
                         <div>
                             <p>{pet.name} eats {recentDiet.amount} of {recentDiet.brand} {recentDiet.frequency},
                                 weighs {recentDiet.weight} lbs, and {age !== null && (
                                     `is ${age} years old.`
-                                )}</p>
-
-                            {isBirthday() && (
-                                <p>Happy Birthday, {pet.name}! 🎉 It's your special day!</p>
-                            )}
-                        </div>
+                                )}</p> */}
+                    {isBirthday() && (
+                        <p>Happy Birthday, {pet.name}! 🎉 It's your special day!</p>
                     )}
+                    {/* </div> */}
+                    {/* )} */}
                 </div>
             )}
 
-            <div className='all-tables'>
+            <div className='diet-table'>
                 <div>
                     <h3>Diet & Weight History</h3>
                     <table>
@@ -224,7 +223,14 @@ const PetDetail = () => {
                     </table>
                 </div>
                 <br />
+            </div>
+            <button onClick={handleAddDietClick} className='button'>
+                <svg className="empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0H24V24H0z"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2zm-3.566 15.604c.881-.556 1.676-1.109 2.42-1.701C18.335 14.533 20 11.943 20 9c0-2.36-1.537-4-3.5-4-1.076 0-2.24.57-3.086 1.414L12 7.828l-1.414-1.414C9.74 5.57 8.576 5 7.5 5 5.56 5 4 6.656 4 9c0 2.944 1.666 5.533 4.645 7.903.745.592 1.54 1.145 2.421 1.7.299.189.595.37.934.572.339-.202.635-.383.934-.571z"></path></svg>
+                <svg className="filled" height="32" width="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0H24V24H0z" fill="none"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"></path></svg>
+                Add Diet & Weight
+            </button>
 
+            <div className='health-tables'>
                 <div>
                     <h3>Veterinary Visit History</h3>
                     <table>
@@ -414,8 +420,12 @@ const PetDetail = () => {
                 </div>
             </div>
 
-            <button onClick={handleAddDietClick} className='button'>Add Diet and Weight</button>
-            <button onClick={handleAddVetConsultClick} className='button'>Add Vet Visit Details</button>
+            <button onClick={handleAddVetConsultClick} className='button'>
+                <svg className="empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0H24V24H0z"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2zm-3.566 15.604c.881-.556 1.676-1.109 2.42-1.701C18.335 14.533 20 11.943 20 9c0-2.36-1.537-4-3.5-4-1.076 0-2.24.57-3.086 1.414L12 7.828l-1.414-1.414C9.74 5.57 8.576 5 7.5 5 5.56 5 4 6.656 4 9c0 2.944 1.666 5.533 4.645 7.903.745.592 1.54 1.145 2.421 1.7.299.189.595.37.934.572.339-.202.635-.383.934-.571z"></path></svg>
+                <svg className="filled" height="32" width="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0H24V24H0z" fill="none"></path><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"></path></svg>
+                Add Health Details
+            </button>
+
         </div>
     )
 }
